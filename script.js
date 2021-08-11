f57e1a2bbbb345c76b55da34d6d8194f590ebf1f
evandrocsjr <evandrocunhajunior@gmail.com>
2021-08-11 13:57:18 -0300

    att

---
+++ script.js
@@ -0,0 +1,46 @@
+// Inicial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
+}
+let player = ''
+let warning = ''
+let playing = false
+
+reset()
+// Events
+document.querySelector('.reset').addEventListener('click', reset)
+
+// Functions
+
+function reset(){
+    warning = ''
+
+    let random = Math.floor(Math.random() * 2)
+    player = (random === 0) ? 'x' : 'o'
+
+    
+    for(let i in square){
+        square[i] = ''
+    }
+
+    playing = true
+
+    renderSquare()
+    renderInformation()
+}
+
+function renderSquare(){
+    for(let i in square){
+        let position = document.querySelector(`div[data-area="${i}"]`)
+        position.innerHTML = square[i]
+
+    }
+}
+
+function renderInformation(){
+    document.querySelector('.infoCorpoVez').innerHTML = player
+    document.querySelector('.infoCorpoResultado').innerHTML = warning
+
+}
